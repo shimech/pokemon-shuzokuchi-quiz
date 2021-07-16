8f936bec4e8b546fce6765d8f12d41502ee9d6cc
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-07-16 15:35:15 +0900

    スタイル修正

--- src/components/molecules/RegionConditionPanel/RegionConditionPanel.tsx
+++ src/components/molecules/RegionConditionPanel/RegionConditionPanel.tsx
@@ -7,19 +7,25 @@ import { RegionName } from '@/store/condition';
 // eslint-disable-next-line @typescript-eslint/ban-types
 type Props = {};
 
-const style = css`
+const wrapperStyle = css`
+  display: flex;
+  justify-content: center;
+  margin-bottom: 12px;
+`;
+
+const gridStyle = css`
   display: grid;
   grid-gap: 12px;
   grid-template-rows: 40px 40px;
   grid-template-columns: 120px 120px 120px 120px;
-  margin: 0 max(calc((100vw - 516px) / 2), 282px);
-  margin-bottom: 12px;
 `;
 
 export const RegionConditionPanel: React.VFC<Props> = () => (
-  <div className={style}>
-    {Object.keys(REGION).map((region) => (
-      <RegionCondition region={region as RegionName} key={region} />
-    ))}
+  <div className={wrapperStyle}>
+    <div className={gridStyle}>
+      {Object.keys(REGION).map((region) => (
+        <RegionCondition region={region as RegionName} key={region} />
+      ))}
+    </div>
   </div>
 );
