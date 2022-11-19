28093e40c1c408ecde7639e02c1fa8bcb6e83067
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2022-11-19 22:23:35 +0900

    migrate

--- bin/migrate.sh
+++ bin/migrate.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-JSON_URI="https://raw.githubusercontent.com/shimech/pokemon-db-maker/master/output/pokemon_db.json"
+JSON_URI="https://raw.githubusercontent.com/shimech/pokemon-db-maker/main/output/pokemon_db.json"
 
 wget -O ./public/api/pokemons.json ${JSON_URI}
